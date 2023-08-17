02321da65cfd226b917aaac7fce2cb1853a0a6e9
anuj1113 <anuj.manchanda92@gmail.com>
2023-08-17 20:46:56 +0000

    first comit

---
+++ classifier.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+df= pd.read_csv("/content/mlops1/data/iris.csv")
+features = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+target = 'species'
+
+X_train, X_test, y_train, y_test = train_test_split(df[features],df[target], test_size=0.3, shuffle=True)
+clf = DecisionTreeClassifier(criterion="entropy")
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+
+print(f"Accuracy of the model is {accuracy_score(y_test,y_pred)*100}")
